8e3779b8c51678a20241743d4a9b1220dfdc9ff4
mdecycu <mdecycuorg@gmail.com>
2022-09-07 21:13:08 +0800

    add Brython page

--- init.py
+++ init.py
@@ -10,10 +10,10 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "cmsimde_site"
+    site_title = "scrum1 on replit"
     ip = "127.0.0.1"
-    dynamic_port = 9443
-    static_port = 8443
+    dynamic_port = 9445
+    static_port = 8445
     def __init__(self):
         # hope to create downloads and images directories　
         if not os.path.isdir(_curdir + "/downloads"):
